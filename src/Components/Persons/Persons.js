9103915bf2b5c16fe553c73e7353dbc68fe36ac8
richardtanu <tanugraha.richard@gmail.com>
2020-06-10 19:24:11 +0700

    8.92

--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,20 +1,31 @@
 import React, { Component }from 'react';
 import Person from './Person/Person';
 class Persons extends Component {
-    static getDerivedStateFromProps(props, state) {
-        console.log('[Persons.js] get DerivedStateFromProps');
-        return state;
+    constructor(props) { 
+        super(props);
+        this.state = {}
     }
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] get DerivedStateFromProps');
+    //     return state;
+    // }
+    // UNSAFE_componentWillReceiveProps() { 
+    //     console.log('[Persons.js] UNSAFE_componentWillReceiveProps', this.props);
+    // }
     shouldComponentUpdate(nextProps, nextState) {
         console.log('[Persons.js]  shouldComponentUpdate');
         return true;
     }
-    getSnapshotBeforeUpdate() { 
+    getSnapshotBeforeUpdate(prevProps, nextState) { 
         console.log('[Persons.js]  getSnapshotBeforeUpdate');
+        return { message: 'Snapshot!'};
     }
+    // componentWillUpdate(nextProps, nextState, nextContext) { 
 
-    componentDidUpdate(prevProps, prevState) { 
+    // }
+    componentDidUpdate(prevProps, prevState, snapshot) { 
         console.log('[Persons.js]  componentDidUpdate');
+        console.log(snapshot);
     }
     render() { 
         console.log('[Persons.js] rendering...');
